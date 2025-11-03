2e36a4c9c69894218d7d86014dc1360da9fd240a
jackDev221 <jackDev221@126.com>
2025-11-03 10:56:28 +0800

    change mod node_handler bitvm2_handler to pub for docs show (#242)

--- node/src/rpc_service/handler/mod.rs
+++ node/src/rpc_service/handler/mod.rs
@@ -1,6 +1,6 @@
 pub mod bitvm2_handler;
-mod node_handler;
-mod proof_handler;
+pub mod node_handler;
+pub mod proof_handler;
 
 // Re-export all handler functions for better documentation visibility
 pub use bitvm2_handler::*;
